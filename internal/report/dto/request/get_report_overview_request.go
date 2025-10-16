87ed3732acf6114f83f25663e14111cfc7dfc8bb
hung-senbox <mr.hung@senbox.org>
2025-10-16 16:05:07 +0700

    add : felid get report overview

--- internal/report/dto/request/get_report_overview_request.go
+++ internal/report/dto/request/get_report_overview_request.go
@@ -1,6 +1,7 @@
 package request
 
 type GetReportOverViewRequest struct {
-	TermID      string `json:"term_id" binding:"required"`
-	ClassroomID string `json:"classroom_id" binding:"required"`
+	TermID        string `json:"term_id" binding:"required"`
+	ClassroomID   string `json:"classroom_id" binding:"required"`
+	UniqueLangKey string `json:"unique_lang_key" binding:"required"`
 }
